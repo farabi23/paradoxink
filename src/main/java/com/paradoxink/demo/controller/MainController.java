63d5577359ae7bc6470d5648e6c38666c75c8b43
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-05-28 14:58:55 +0500

    testing

---
+++ src/main/java/com/paradoxink/demo/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.paradoxink.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping()
+    public String mainPage (){
+
+        return "hi!";
+    }
+
+}
---
+++ src/main/java/com/paradoxink/demo/model/Task.java
@@ -0,0 +1,4 @@
+package com.paradoxink.demo.model;
+
+public class Task {
+}
